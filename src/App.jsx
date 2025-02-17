e37c30cd03540492e058c174e6f0c05b97c10fa5
Drew <drew@werd.uk>
2025-02-17 15:40:07 +0000

    add base container for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+        <div className="flex-nowrap gap-5 bg-white dark:bg-black min-h-[100dvh] min-w-[100dvw] justify-center p-10">
+            <div className="grid grid-cols-12 gap-1">
+                <header className="flex col-span-full bg-white rounded-md text-black p-5 min-fit">Logo</header>
+                <main className="flex col-span-full bg-white rounded-md text-black p-5 min-fit">Middle</main>
+                <footer className="flex col-span-full bg-white rounded-md text-black p-5 min-fit">Footer</footer>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        </div>
     );
 }
 
