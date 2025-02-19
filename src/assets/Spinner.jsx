b1cc8e02e887f6c01aaab22be5fccdb8d7bf4daa
Drew <drew@werd.uk>
2025-02-19 09:10:58 +0000

    add voting buttons to article

---
+++ src/assets/Spinner.jsx
@@ -0,0 +1,6 @@
+import { CircleNotch } from "@phosphor-icons/react";
+function LoadingSpinner() {
+    return <CircleNotch className="animate-spin min-h-fit self-center text-xl"></CircleNotch>;
+}
+
+export default LoadingSpinner;
