0afdaacd9cdaa53763548f2258472e6c12791c34
Drew <drew@werd.uk>
2025-02-19 17:57:56 +0000

    add validation on zero length input

--- src/components/comments/NewComment.jsx
+++ src/components/comments/NewComment.jsx
@@ -6,7 +6,7 @@ import Notice from "../Notice";
 
 function NewComment({ comments, setComments }) {
     const [tempCommentText, setTempCommentText] = useState("");
-    const [isValid, setIsValid] = useState(false);
+    const [isCommentValid, setIsCommentValid] = useState(false);
     const [currentUser, setCurrentUser] = useState("weegembump");
     const [inputNotice, setInputNotice] = useState({ visible: false, level: "notice", msg: "" });
     const [newCommentLoading, setNewCommentLoading] = useState(false);
@@ -31,6 +31,21 @@ function NewComment({ comments, setComments }) {
     useEffect(() => {
         fecthLatestComments(id);
     }, [newCommentLoading]);
+
+    const validateInput = (e) => {
+        const re = /[^a-zA-Z0-9\t\n ./<>?;:"'`!@*#$%&()\[\]{}_+=|\\-]/u;
+        if (!re.test(e.target.value)) {
+            setInputNotice({ visible: false, level: "", msg: "" });
+            setTempCommentText(e.target.value);
+        } else {
+            setInputNotice({ visible: true, level: "notice", msg: "Character(s) not allowed!" });
+
+            setTimeout(() => {
+                setInputNotice({ visible: false });
+            }, 2000);
+        }
+        setIsCommentValid(e.target.value.length > 0);
+    };
 
     return (
         <>
@@ -59,26 +74,15 @@ function NewComment({ comments, setComments }) {
                             className="w-full px-1 pt-1 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
                             placeholder="Write a comment..."
                             value={tempCommentText}
-                            onChange={(e) => {
-                                const re = /[^a-zA-Z0-9\t\n ./<>?;:"'`!@*#$%&()\[\]{}_+=|\\-]/u;
-                                if (!re.test(e.target.value)) {
-                                    setInputNotice({ visible: false, level: "", msg: "" });
-                                    setTempCommentText(e.target.value);
-                                } else {
-                                    setInputNotice({ visible: true, level: "notice", msg: "Character(s) not allowed!" });
-                                    setTimeout(() => {
-                                        setInputNotice({ visible: false });
-                                    }, 2000);
-                                }
-                            }}
+                            onChange={validateInput}
                             required></textarea>
                     </div>
                     <div className="flex items-center justify-end px-3 py-2 border-t dark:border-gray-600 border-gray-200 gap-2">
                         {inputNotice.visible ? <Notice message={inputNotice.msg} level={inputNotice.level} /> : null}
                         <button
                             type="submit"
-                            disabled={isValid}
-                            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                            disabled={!isCommentValid}
+                            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800 disabled:bg-gray-200 disabled:text-gray-500">
                             Post comment as {currentUser}
                             {newCommentLoading ? <LoadingSpinner /> : null}
                         </button>
