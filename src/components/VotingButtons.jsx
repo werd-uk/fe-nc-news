04366441a5210859bfddb4e3aaaaed59aa7025e1
Drew <drew@werd.uk>
2025-02-19 09:02:04 +0000

    improve the styling on the voting buttons

--- src/components/VotingButtons.jsx
+++ src/components/VotingButtons.jsx
@@ -19,9 +19,9 @@ function VotingButtons({ type, initCount, id, votes, setVotes }) {
     }, []);
 
     return (
-        <div key={id} className="flex bg-gray-400 rounded-sm max-w-max">
+        <div key={id} className="flex bg-black/10 rounded-sm max-w-max">
             <span className="flex gap-2 px-2">
-                <button className="hover:bg-red-500/20 hover:border-1 hover:border-red-500/50 rounded-sm min-w-7 justify-items-center" onClick={() => amendVote(-1)}>
+                <button className="hover:bg-red-500/20 hover:border-1 hover:border-red-500/20 rounded-sm min-w-7 justify-items-center" onClick={() => amendVote(-1)}>
                     <Skull className="rotate-180" color="#bc0000" weight="duotone" size={23} />
                 </button>
                 <div className="w-9 h-10 p-2 rounded-sm">
@@ -33,7 +33,7 @@ function VotingButtons({ type, initCount, id, votes, setVotes }) {
                         </div>
                     )}
                 </div>
-                <button className=" hover:bg-green-500/20 hover:border-1 hover:border-green-800/50 rounded-sm min-w-7 justify-items-center" onClick={() => amendVote(1)}>
+                <button className=" hover:bg-green-500/20 hover:border-1 hover:border-green-800/20 rounded-sm min-w-7 justify-items-center" onClick={() => amendVote(1)}>
                     <Skull color="#015607" weight="duotone" size={23} />
                 </button>
             </span>
--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -34,7 +34,7 @@ function SingleArticle() {
                     <img className="object-cover rounded-lg" src={currentArticle.article_img_url} />
                 </div>
                 <div className="order-1 col-span-full text-3xl">{currentArticle.title}</div>
-                <div className="order-3 col-span-full sm:justify-self-end md:justify-self-start">
+                <div className="order-3 md:col-span-full md:justify-self-start justify-self-end">
                     <VotingButtons type="article" id={currentArticle.article_id} initCount={currentArticle.votes} setVotes={setArticleVotes} votes={articleVotes} />
                 </div>
                 <div className="order-4 col-span-full">{currentArticle.body}</div>
