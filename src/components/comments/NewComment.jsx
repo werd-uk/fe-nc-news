8b01907d867f39442fb6d44f5bcb227b9fb00848
Drew <drew@werd.uk>
2025-02-19 17:13:04 +0000

    improve styling of input box

--- src/components/comments/NewComment.jsx
+++ src/components/comments/NewComment.jsx
@@ -47,14 +47,14 @@ function NewComment({ comments, setComments }) {
                         });
                 }}>
                 <div className="xl:w-[50vw] mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-                    <div className="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+                    <div className="px-2 py-2 bg-white rounded-t-lg dark:bg-gray-800">
                         <label htmlFor="comment" className="sr-only">
                             Your comment
                         </label>
                         <textarea
                             id="comment"
                             rows="4"
-                            className="w-full px-0 pt-1 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                            className="w-full px-1 pt-1 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
                             placeholder="Write a comment..."
                             value={tempCommentText}
                             onKeyDown={(e) => {
